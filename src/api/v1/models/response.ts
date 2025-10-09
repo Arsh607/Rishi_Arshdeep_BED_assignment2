52210049236efd3cfb2abbe8bc3269157ec507d5
rrc_f2024_arishi <arishi@academic.rrc.ca>
2025-10-08 19:54:56 -0500

    feat: Added an error response to the response model.

--- src/api/v1/models/response.ts
+++ src/api/v1/models/response.ts
@@ -1,3 +1,7 @@
 export type Status = "success" | "error";
 export interface ApiResponse<T> { status: Status; data?: T; message?: string; error?: string; }
 export const successResponse = <T>(data?: T, message?: string): ApiResponse<T> => ({ status: "success", data, message });
+export const errorResponse = (message: string) => ({
+  status: "error",
+  error: message
+});
