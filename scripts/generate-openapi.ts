c53bb2a58c1fe3e00c70b15e411b08cfe3b7a3e2
rrc_f2024_arishi <arishi@academic.rrc.ca>
2025-11-05 22:36:28 -0600

    feat: Created script to generate docs.

---
+++ scripts/generate-openapi.ts
@@ -0,0 +1,5 @@
+import fs from "fs";
+import { generateSwaggerSpec } from "../src/config/swaggerOptions";
+const specs = generateSwaggerSpec();
+fs.writeFileSync("./openapi.json", JSON.stringify(specs, null, 2));
+console.log("OpenAPI specification generated successfully!");
