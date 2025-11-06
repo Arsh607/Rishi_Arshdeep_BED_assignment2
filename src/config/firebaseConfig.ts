b9e1bbdb4dca493412932f9e3db6381c09add27e
rrc_f2024_arishi <arishi@academic.rrc.ca>
2025-11-05 23:40:51 -0600

    fix: Updated issues in code.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,19 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { initializeApp, cert, getApps, App, AppOptions } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 const getFirebaseConfig = (): AppOptions => {
   const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env;
-  if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY)
+
+  if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
     throw new Error("Missing Firebase configuration.");
+  }
+
+  const privateKey = FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
   return {
     credential: cert({
       projectId: FIREBASE_PROJECT_ID,
       clientEmail: FIREBASE_CLIENT_EMAIL,
-      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey,
     }),
   };
 };
 
-const app: App = getApps()[0] || initializeApp(getFirebaseConfig());
-export const db = getFirestore(app);
+const firebaseApp: App = getApps().length === 0 ? initializeApp(getFirebaseConfig()) : getApps()[0];
+
+export const db = getFirestore(firebaseApp);
